c6f92c9852f4c81c7b8efaedb42759f5ace5a4e3
fl05an17 <31561075+fl05an17@users.noreply.github.com>
2019-11-09 17:09:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/noguiapp20/ActivityProfile.kt
@@ -0,0 +1,12 @@
+package com.example.noguiapp20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
